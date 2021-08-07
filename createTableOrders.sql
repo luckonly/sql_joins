3d8d6ecf7e8853d1172c4dd2df2a589aba7ba851
luckonly <61157898+luckonly@users.noreply.github.com>
2021-08-07 23:27:12 +0300

    Added schema, data

--- createTableOrders.sql
+++ createTableOrders.sql
@@ -1,8 +1,16 @@
-create table netology.orders(
-    id int primary key auto_increment,
-    date timestamp default now(),
-    customer_id int not null,
+CREATE SCHEMA if not exists netology;
+
+CREATE TABLE if not exists netology.orders
+(
+    id           int primary key,
+    date         timestamp default now(),
+    customer_id  int          not null,
     product_name varchar(255) not null,
-    amount int not null,
-    FOREIGN KEY (customer_id) REFERENCES customers (id)
-)
+    amount       int          not null,
+    FOREIGN KEY (customer_id) REFERENCES netology.customers (id)
+    );
+
+INSERT INTO netology.orders(id, customer_id, product_name, amount)
+VALUES ('1', '1', 'Samsung Galaxy S20', '1');
+INSERT INTO netology.orders(id, customer_id, product_name, amount)
+VALUES ('2','2', 'iPhone 12 PRO MAX', '1');
