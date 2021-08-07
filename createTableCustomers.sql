3ef4a7dfec168353c6f040add58d7230c732197c
luckonly <61157898+luckonly@users.noreply.github.com>
2021-08-07 23:26:29 +0300

    Added schema, data

--- createTableCustomers.sql
+++ createTableCustomers.sql
@@ -1,8 +1,15 @@
-create table netology.customers(
-    id int primary key auto_increment,
-    name varchar(255) not null,
-    surname varchar(255) not null,
-    age int check (age > 0),
-    phone_number varchar(255)
-);
+CREATE SCHEMA if not exists netology;
 
+CREATE TABLE if not exists netology.customers
+(
+    id           int primary key,
+    name         varchar(255) not null,
+    surname      varchar(255) not null,
+    age          int check (age > 0),
+    phone_number varchar(255)
+    );
+
+INSERT INTO netology.customers(id, name, surname, age, phone_number)
+VALUES ('1', 'Alexey', 'Ivanov', '28', '375298876754');
+INSERT INTO netology.customers(id, name, surname, age, phone_number)
+VALUES ('2', 'Dmitry', 'Petrov', '25', '375295649374');
